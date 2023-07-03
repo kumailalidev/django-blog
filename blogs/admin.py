e94d380d2149fc461e48408f0d01fadac5855722
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-07-03 20:22:41 +0500

    48-Prepopulate Slugs.

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Category, Blog
 
+
+class BlogAdmin(admin.ModelAdmin):
+    """Admin class for blog model."""
+
+    prepopulated_fields = {
+        "slug": ("title",),
+    }
+
+
 admin.site.register(Category)
-admin.site.register(Blog)
+admin.site.register(Blog, BlogAdmin)
