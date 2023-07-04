f952a9c58fbfb19f852a65e4c5a58e81389cd4e9
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-07-05 00:47:27 +0500

    57-Post By Category Layout and Try Except Block

--- blogs/views.py
+++ blogs/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 
-from .models import Blog
+from .models import Blog, Category
 
 
 def posts_by_category(request, category_id):
@@ -9,8 +9,14 @@ def posts_by_category(request, category_id):
     Fetch the posts that belongs to the category with the id category_id
     """
     posts = Blog.objects.filter(status="Published", category=category_id)
+    try:
+        category = Category.objects.get(pk=category_id)
+    except:
+        return redirect("home")
+
     context = {
         "posts": posts,
+        "category": category,
     }
 
     return render(request, "posts_by_category.html", context)
