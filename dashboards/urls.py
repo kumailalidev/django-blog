18eac7cbab0bea8d0c12c8999b613768f17ae45c
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-07-14 00:03:34 +0500

    95-Editing blog post

--- dashboards/urls.py
+++ dashboards/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path("categories/delete/<int:pk>/", views.delete_category, name="delete_category"),
     path("posts/", views.posts, name="posts"),
     path("posts/add/", views.add_post, name="add_post"),
+    path("posts/edit/<int:pk>/", views.edit_post, name="edit_post"),
 ]
