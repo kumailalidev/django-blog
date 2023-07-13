c1c538521bbcae8921f0f78e496beb5d01988047
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-07-13 17:39:42 +0500

    82-Blogs and category count

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from blogs.models import Category, Blog
 
 
 def dashboard(request):
-    return render(request, "dashboard/dashboard.html")
+    category_count = Category.objects.all().count()
+    blogs_count = Blog.objects.all().count()
+    context = {
+        "category_count": category_count,
+        "blogs_count": blogs_count,
+    }
+    return render(request, "dashboard/dashboard.html", context)
