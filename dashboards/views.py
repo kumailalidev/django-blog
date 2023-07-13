92bb50d8d33b43966766f8eccb4cb3a3fbee50b0
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-07-13 17:45:23 +0500

    83-Login required decorator

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from blogs.models import Category, Blog
+from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url="login")
 def dashboard(request):
     category_count = Category.objects.all().count()
     blogs_count = Blog.objects.all().count()
