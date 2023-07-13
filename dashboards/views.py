b3c5cd96d5ee4789ccbf54c5e243132eeece4ed8
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-07-14 00:12:15 +0500

    96-Delete post

--- dashboards/views.py
+++ dashboards/views.py
@@ -104,3 +104,9 @@ def edit_post(request, pk):
     }
 
     return render(request, "dashboard/edit_post.html", context)
+
+
+def delete_post(request, pk):
+    post = get_object_or_404(Blog, pk=pk)
+    post.delete()
+    return redirect("posts")
