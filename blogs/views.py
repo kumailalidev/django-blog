62dbc7221baafdd29fda5418a1ce30e47e102b9f
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-07-14 17:58:36 +0500

    110-Add comment

--- blogs/views.py
+++ blogs/views.py
@@ -1,4 +1,5 @@
 from django.db.models import Q
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, redirect, render
 
 from .models import Blog, Category, Comment
@@ -27,12 +28,23 @@ def posts_by_category(request, category_id):
 
 def blogs(request, slug):
     single_blog = get_object_or_404(Blog, slug=slug, status="Published")
+
+    if request.method == "POST":
+        comment = Comment()
+        comment.user = request.user
+        comment.blog = single_blog
+        comment.comment = request.POST["comment"]  # name attribute of textarea
+        comment.save()
+        return HttpResponseRedirect(request.path_info)
+
     comments = Comment.objects.filter(blog=single_blog)
+
     context = {
         "single_blog": single_blog,
         "comments": comments,
         "comment_count": comments.count(),
     }
+
     return render(request, "blogs.html", context)
 
 
